dfe90a17367c326be8f0fd08f7965b9a6a0274a6
DivyaAhire11 <divyaahire121@gmail.com>
2025-12-16 17:13:56 +0530

    Mouse all 7 Methods

--- MyMouselistener.java
+++ MyMouselistener.java
@@ -9,6 +9,14 @@
  * MOUSE MOTION LISTENER :
  *              void mouseDragged(MouseEvent e)
                 void mouseMoved(MouseEvent e)
+
+ * Mouse generally move using X,Y coordinated
+        ---->  m.getX() : X coordinates
+        ---->  m.getY() : Y coordinates
+        ---->  m.getPoint()  : Returns a Point object with (x,y)
+        ---->  m.getClickCount()  : Returns number of clicks (useful for double-click detection)
+        ---->  m.getButton()  :  Returns which mouse button was pressed (LEFT, MIDDLE, RIGHT)
+        
  */
 
 
@@ -31,27 +39,27 @@ public class MyMouselistener extends JFrame implements MouseListener,MouseMotion
     }
 
     public void mouseEntered(MouseEvent m){
-        System.out.println("Mouse Entered");
+        System.out.println("Mouse Entered :: "+m.getX()+" : "+m.getY());
     }
     public void mouseExited(MouseEvent m){
-       System.out.println("Mouse Exited");
+       System.out.println("Mouse Exited :: "+m.getPoint());
     }
     public void mousePressed(MouseEvent m){  
-       System.out.println("Mouse Pressed");
+       System.out.println("Mouse Pressed :: "+m.getButton());
     }
     public void mouseReleased(MouseEvent m){
-       System.out.println("Mouse Released");
+       System.out.println("Mouse Released ");
     }
     public void mouseClicked(MouseEvent m){
-       System.out.println("Mouse Click");
+       System.out.println("Mouse Click :: "+m.getClickCount());
     }
     public void mouseDragged(MouseEvent me){
-        System.out.println("Mouse Dragged");
+        System.out.println("Mouse Dragged ");
     }
     public void mouseMoved(MouseEvent me){
-        System.out.println("Mouse Moved");
+        // System.out.println("Mouse Moved ");
     }
-    
+
     public static void main(String[] args) {
         new MyMouselistener();
     }
