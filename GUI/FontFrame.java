4228a47b8737abcb29fe387c07b06d743345f1f8
DivyaAhire11 <divyaahire121@gmail.com>
2025-12-17 18:35:42 +0530

    Beautiful Frame Craft

--- GUI/FontFrame.java
+++ GUI/FontFrame.java
@@ -12,7 +12,7 @@ class FontFrame extends JFrame{
     JPanel main,bottom,left,right;
     JLabel l1,l2,l3;
     JComboBox cb1,cb2;
-    JCheckBox b1,b2;
+    JCheckBox b1,b2,b3;
     JTextField tf;
     FontFrame(){
         setTitle("Font frame");
@@ -27,17 +27,18 @@ class FontFrame extends JFrame{
                 left.setBorder(BorderFactory.createLineBorder(Color.RED,2));
                 l1 = new JLabel("Font : ");
                 cb1 = new JComboBox<>();
-                   cb1.addItem("hii");
-                   cb1.addItem("hii");
-                   cb1.addItem("hii");
-                   cb1.addItem("hii");
+                   cb1.addItem("Arial");
+                   cb1.addItem("Serif");
+                   cb1.addItem("SansSerif");
+                   cb1.addItem("Monospaced");
+                   cb1.addItem("Dialog");
                 l2 = new JLabel("Size : ");
                 cb2 = new JComboBox<>();
-                    cb2.addItem("Byyy");
-                    cb2.addItem("Byyy");
-                    cb2.addItem("Byyy");
-                    cb2.addItem("Byyy");
-                    cb2.addItem("Byyy");
+                    cb2.addItem("10");
+                    cb2.addItem("20");
+                    cb2.addItem("30");
+                    cb2.addItem("40");
+                    cb2.addItem("50");
 
                     left.add(l1);
                     left.add(cb1);
@@ -45,17 +46,20 @@ class FontFrame extends JFrame{
                     left.add(cb2);
 
              main.add(left);
-            //Right Panel
-            right = new JPanel(new GridLayout(3,1));
+         
+             //Right Panel
+            right = new JPanel(new GridLayout(4,1));
                right.setBorder(BorderFactory.createLineBorder(Color.BLUE,2));
 
                l3 = new JLabel("Style : ");
                b1 = new JCheckBox("Bold");
                b2 = new JCheckBox("Italic");
+               b3 = new JCheckBox("Underline");
 
                 right.add(l3);
                 right.add(b1);
                 right.add(b2);
+                right.add(b3);
             main.add(right);
        
        
@@ -73,6 +77,7 @@ class FontFrame extends JFrame{
         
          setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+        pack();
     }
     public static void main(String[] args) {
         new FontFrame();
