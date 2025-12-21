1a22b7a710d6f74f70c958e1f879440a13758af0
DivyaAhire11 <divyaahire121@gmail.com>
2025-12-21 11:14:23 +0530

    About Panel

--- GUISwing/MyPanel.java
+++ GUISwing/MyPanel.java
@@ -1,31 +1,48 @@
 package GUISwing;
 
-import java.awt.Color;
-
+import java.awt.*; //Color, Image
 import javax.swing.*; //JFrame , JPanel , Jlabel , ImageIcon ,
 
 public class MyPanel extends JFrame {
-    JPanel p1,p2,p3;
+    JPanel p1, p2, p3;
+    JLabel l;
+    ImageIcon img;
+
     MyPanel() {
+        ImageIcon afraidimg = new ImageIcon(
+                new ImageIcon("wow.png").getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
+
         setTitle("All About TextField");
-        
+        l = new JLabel(afraidimg); // l.setIcon(img)
+        //l.setText("hello");
+       // l.setVerticalAlignment(JLabel.BOTTOM); // no need when we no used any layout
+       //  l.setHorizontalAlignment(JLabel.RIGHT); 
+       l.setBounds(50,50,100,100);  
+
+
         p1 = new JPanel();
         p1.setBackground(Color.red);
-        p1.setBounds(0,0,250,250);
-        
+        p1.setBounds(0, 0, 250, 250);
+        p1.setLayout(null);
+
         p2 = new JPanel();
         p2.setBackground(Color.BLUE);
-        p2.setBounds(250,0,250,250);
-        
+        p2.setBounds(250, 0, 250, 250);
+        p2.setLayout(null);
+
         p3 = new JPanel();
         p3.setBackground(Color.green);
-        p3.setBounds(0,250,500,250);
-        
+        p3.setBounds(0, 250, 500, 250);
+        p3.setLayout(null);
+       
+        p3.add(l);
+
         add(p1);
         add(p2);
         add(p3);
+
         setLayout(null);
-        setSize(750,750);
+        setSize(750, 750);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
